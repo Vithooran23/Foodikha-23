f69a6b372bd1945e36868ecc7362c0f1e8a28312
Kandeeparaja Vithooran <vithoo24@gmail.com>
2024-03-10 22:33:23 +0530

    Added04

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect} from 'react'
-// import { useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Login from "../components/Login/Login";
-// import SuggestedProduct from '../components/Products/SuggestedProduct';
+
 
 
 
 
 const LoginPage = () => {
-  // const navigate = useNavigate();
-  // const { isAuthenticated } = useSelector((state) => state);
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state);
 
-  // useEffect(() => {
-  //   if(isAuthenticated === true){
-  //     navigate("/");
-  //   }
-  // }, [])
+  useEffect(() => {
+    if(isAuthenticated === true){
+      navigate("/");
+    }
+  }, [])
   
   return (
     <div>
